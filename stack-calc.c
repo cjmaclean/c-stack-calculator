4aac448b9c9f0c90a640b7de88344358154a43c1
Caspian Maclean <caspianmaclean@gmail.com>
2022-02-14 11:13:08 +0800

    implement function to add top values on stack.

--- stack-calc.c
+++ stack-calc.c
@@ -13,11 +13,28 @@ void user_pop(stack *s) {
     }
 }
 
+void cmd_add(stack *s) {
+    if (empty(*s)) {
+        printf("Stack is empty, cannot add values\n");
+        return;
+    }
+    int value1 = pop(s);
+    if (empty(*s)) {
+        printf("Stack insufficient, cannot add values\n");
+        return;
+    }
+    int value2 = pop(s);
+    push(s, value1+value2);
+}
+
 int main() {
     printf("starting\n\n");
 
-    push(&user_stack, 5);
+    push(&user_stack, 2);
+    push(&user_stack, 15);
     push(&user_stack, 3);
+
+    cmd_add(&user_stack);
     
     user_pop(&user_stack);
     user_pop(&user_stack);
