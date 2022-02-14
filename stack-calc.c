e099ce4a713d884bc147606ae3c003395c05d3f0
Caspian Maclean <caspianmaclean@gmail.com>
2022-02-14 10:11:14 +0800

    define stack type

--- stack-calc.c
+++ stack-calc.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
+struct stack_element {
+    int item;
+    struct stack_element *next;
+};
+
+typedef struct stack_element *stack;
+
 int main() {
     printf("hello\n");
     return 0;
