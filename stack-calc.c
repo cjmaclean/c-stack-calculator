93c4653cd358ae6ea37f607d837d641cd5da5a3f
Caspian Maclean <caspianmaclean@gmail.com>
2022-02-14 11:05:59 +0800

    Put stack code into separate .c and .h files from the main code.

--- stack-calc.c
+++ stack-calc.c
@@ -1,47 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
-
-/* Stack type */
-
-struct stack_element {
-    int item;
-    struct stack_element *next;
-};
-
-typedef struct stack_element *stack;
-
-void push(stack *s, int value) {
-    struct stack_element *new_stack_element;
-    new_stack_element = malloc(sizeof(struct stack_element));
-    if(!new_stack_element) {
-        printf("failed to allocate\n");
-        return;
-    } else {
-        new_stack_element->item = value;
-        new_stack_element->next = *s;
-        *s = new_stack_element;
-    }
-}
-
-int empty(stack s) {
-    return !s;
-}
-
-int pop(stack *s) {
-    if (empty(*s)) {
-        printf("failed to pop\n");
-        return 0;
-    } else {
-        int value = (*s)->item;
-        struct stack_element *next_stack_element;
-        next_stack_element = (*s)->next;
-        free(*s);
-        *s = next_stack_element;
-        return value;
-    }
-}
-
-/* end stack type */
+#include "intstack.h"
 
 stack user_stack = NULL;
 
