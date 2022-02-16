58621e7cd80327fa195b82a83d3f0d50c58d3543
Caspian Maclean <caspianmaclean@gmail.com>
2022-02-16 17:51:00 +0800

    Comment memory management

--- intstack.c
+++ intstack.c
@@ -4,8 +4,10 @@
 
 /* Stack type functions */
 
+
 void push(stack *s, int value) {
     struct stack_element *new_stack_element;
+    // malloc called here, corresponding free called in pop 
     new_stack_element = malloc(sizeof(struct stack_element));
     if(!new_stack_element) {
         printf("failed to allocate\n");
@@ -26,9 +28,12 @@ int pop(stack *s) {
         printf("failed to pop\n");
         return 0;
     } else {
+        // store data from top element of stack before freeing
         int value = (*s)->item;
         struct stack_element *next_stack_element;
         next_stack_element = (*s)->next;
+        // free called here, corresponding
+        // malloc called in push 
         free(*s);
         *s = next_stack_element;
         return value;
